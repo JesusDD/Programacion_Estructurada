0e3ca7d8e0cd57a3d85b19971651a0e27267c977
Francisco <pacos122a@gmail.com>
2018-01-29 00:42:37 -0600

    Ejercicios 6, 18, 48 añadidos

---
+++ 3Source-6.c
@@ -0,0 +1,29 @@
+nclude <stdio.h>
+	
+	int main(int argc, char *argv[]) {
+	int catetoA = 0;
+	int catetoB = 0;
+	float hipotenusa = 0;
+	char *resultado = "a";
+	int potenciaAB = 0;
+	float potenciaC = 0;
+	/*Entradas*/
+	printf("%s", "ingrese el valor del cateto opuesto ");
+	scanf("%i", &catetoA);
+	printf("%s", "ingrese el valor del cateto adjasente ");
+	scanf("%i", &catetoB);
+	printf("%s", "ingrese el valor de la hipotenusa ");
+	scanf("%i", &hipotenusa);
+	/*Procesos*/
+	potenciaAB = (catetoA*catetoA) + (catetoB*catetoB);
+	potenciaC = hipotenusa*hipotenusa;
+	if(potenciaAB==potenciaC){
+		resultado = "cumple con la ecuaccion a^2 + b^2 = c^2";
+	}
+	else{
+		resultado = "no cumple con la ecuaccion a^2 + b^2 = c^2";
+	};
+	/*Salidas*/
+	printf("%s", resultado);
+	return 0;
+}
